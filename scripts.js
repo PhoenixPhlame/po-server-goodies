a9759622f89e27913aea37eda271fe388a298517
PhoenixPhlame <internalvortex@gmail.com>
2014-06-19 17:54:07 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,8 +3,7 @@
 /*jshint "laxbreak":true,"shadow":true,"undef":true,"evil":true,"trailing":true,"proto":true,"withstmt":true*/
 // You may change these variables as long as you keep the same type
 var Config = {
-    base_url: "https://raw.github.com/PhoenixPhlame/po-server-goodies/master/",
-    dataDir: "scriptdata/",
+    base_url: "https://raw.githubusercontent.com/PhoenixPhlame/po-server-goodies/master/",
     bot: "Dratini",
     kickbot: "Blaziken",
     capsbot: "Exploud",
